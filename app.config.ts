adaeb01d47bd052bd59b670e1a246d430877c182
Avalynn <100826194+avalynndev@users.noreply.github.com>
2023-02-05 15:34:41 +0000

    Basic COde

---
+++ app.config.ts
@@ -0,0 +1,31 @@
+export default defineAppConfig({
+  docus: {
+    title: 'Easyscript.ts',
+    description: 'Npm package which makes coding in Typescript easy!.',
+    socials: {
+      twitter: 'EasyScriptJS',
+      github: 'EasyScriptJS',
+    },
+    aside: {
+      level: 0,
+      exclude: []
+    },
+    header: {
+      showLinkIcon: true,
+      exclude: []
+    },
+    github: {
+      owner: "EasyScriptJS",
+      branch: "main",
+      repo: "ts",
+      edit: true,
+    },
+    footer: {
+      credits: {
+        icon: 'IconDocus',
+        text: 'Created with ❤️ by Avalynndev',
+        href: 'https://github.com/avalynndev',
+      },
+    }
+  }
+})
